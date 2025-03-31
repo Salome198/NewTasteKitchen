28a1376d2e2e45eb041bddeead9a2327a9ca7c2c
Salome198 <s.madubuko@wlv.ac.uk>
2025-03-31 23:23:16 +0100

    Update NewsModel.php
    workshop practice

--- app/Models/NewsModel.php
+++ app/Models/NewsModel.php
@@ -7,6 +7,7 @@ use CodeIgniter\Model;
 class NewsModel extends Model
 {
     protected $table = 'news';
+    protected $allowedFields = ['title', 'slug', 'body'];
        /**
      * @param false|string $slug
      *
@@ -20,4 +21,6 @@ class NewsModel extends Model
 
         return $this->where(['slug' => $slug])->first();
     }
+
+
 }
