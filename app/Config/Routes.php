86e1810bf9494d2801c9255ba6f05a45d32227da
Salome198 <s.madubuko@wlv.ac.uk>
2025-03-31 23:38:50 +0100

    Update Routes.php
    New routes were created for the pages and reviews controller, making sure the right connections were being made, especially for the review form when fetching and storing data dynamically

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,14 +3,25 @@
 use CodeIgniter\Router\RouteCollection;
 use App\Controllers\Pages;
 use App\Controllers\News;
+use App\Controllers\ReviewsController;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
 
- $routes->get('news', [News::class, 'index']);           // Add this line
- $routes->get('news/(:segment)', [News::class, 'show']); // Add this line
+ $routes->get('news', [News::class, 'index']);    
+ $routes->get('news/new', [News::class, 'new']); 
+ $routes->post('news', [News::class, 'create']);   
+ $routes->get('news/(:segment)', [News::class, 'show']); 
+
+ $routes->get('review/create', [ReviewsController::class, 'create']); 
+ $routes->post('review/create', [ReviewsController::class, 'create']); 
+ $routes->get('review', [ReviewsController::class, 'submitReview']);
+ $routes->get('review/getReviews',[ReviewsController:: class, 'getReviews']);
+
  
  $routes->get('pages', [Pages::class, 'index']);
+ 
+ $routes->get('menu', [Pages::class, 'menupage']);
  $routes->get('(:segment)', [Pages::class, 'view']);
